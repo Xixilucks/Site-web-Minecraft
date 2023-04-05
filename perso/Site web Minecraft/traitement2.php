3459c2a4ac0b4d6fccfec9711637e872cf2be4b3
Xixilucks <127399656+Xixilucks@users.noreply.github.com>
2023-04-05 22:15:35 +0200

    dd

--- perso/Site web Minecraft/traitement2.php
+++ perso/Site web Minecraft/traitement2.php
@@ -4,107 +4,15 @@
     <meta charset="UTF-8">
     <link rel="shortcut icon" type="image/png" href="tete.png"/>
     <title>Minecraft Manship Wiki</title>
-    <style>
-        #connexion-section {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            }
-
-        #connexion-section {
-            backdrop-filter: blur(5px);
-            background-color: rgba(0, 0, 0, 0.5);
-            }
-
-        
-        label{
-            color: white;
-            }
-        
-
-        h2 {
-            text-align: center;
-            font-size: 2em;
-            margin-top: 0;
-            margin-bottom: 20px;
-            color: white;
-            }
-        
-        body{
-            background-image: url("fond_login.png");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-            font-family: 'Roboto', sans-serif;
-            background-attachment: fixed;
-            color: white;
-            }
-        a {
-            background-color: black;
-            color: white;
-            border-radius: 20px; 
-            padding: 10px 20px; 
-            border: none;
-            text-decoration: none;
-            }
-
-        a:hover {
-            background-color: white;
-            color: black;
-            border-radius: 20px; 
-            padding: 10px 20px; 
-            border: none;
-            text-decoration: none;
-            }
-
-        form{
-            text-align: center;
-            align-items: center;
-        }
-        
-    </style>
+    <link rel="stylesheet" href="styless.css">
 </head>
 <body>
-    <div id="connexion-section">
-    <?php
-// Se connecter à la base de données
-$conn = mysqli_connect("localhost", "nom_utilisateur", "mot_de_passe", "nom_de_la_base_de_donnees");
-
-// Vérifier la connexion
-if (!$conn) {
-    die("La connexion à la base de données a échoué : " . mysqli_connect_error());
-}
-
-// Vérifier si le formulaire a été soumis
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    // Récupérer les valeurs du formulaire
-    $nom = mysqli_real_escape_string($conn, $_POST['nom']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $mot_de_passe = mysqli_real_escape_string($conn, $_POST['mot_de_passe']);
-
-    // Vérifier si l'utilisateur existe déjà dans la base de données
-    $result = mysqli_query($conn, "SELECT * FROM utilisateurs WHERE email='$email'");
-    $count = mysqli_num_rows($result);
-
-    if ($count == 0) {
-        // Ajouter l'utilisateur à la base de données
-        $sql = "INSERT INTO utilisateurs (nom, email, mot_de_passe) VALUES ('$nom', '$email', '$mot_de_passe')";
-        
-        if (mysqli_query($conn, $sql)) {
-            echo "L'utilisateur a été ajouté avec succès à la base de données.";
-        } else {
-            echo "Une erreur s'est produite : " . mysqli_error($conn);
-        }
-    } else {
-        echo "L'utilisateur existe déjà dans la base de données.";
-    }
-}
-
-// Fermer la connexion
-mysqli_close($conn);
-?>
-    </div>
+   <form>
+    <fieldset>
+            <h2>Inscription réussi !!</h2>
+           <p>Vous êtes désormais inscrit sur notre site.</p>
+           <button><a href="Page accueil 2.html">Retourner a l'accueil</a></button>
+     </fieldset>
+    </form>
 </body>
 </html>
--- perso/Site web Minecraft/traitemet.php
+++ perso/Site web Minecraft/traitemet.php
@@ -4,105 +4,16 @@
     <meta charset="UTF-8">
     <link rel="shortcut icon" type="image/png" href="tete.png"/>
     <title>Minecraft Manship Wiki</title>
-    <style>
-        #connexion-section {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            }
-
-        #connexion-section {
-            backdrop-filter: blur(5px);
-            background-color: rgba(0, 0, 0, 0.5);
-            }
-
-        
-        label{
-            color: white;
-            }
-        
-
-        h2 {
-            text-align: center;
-            font-size: 2em;
-            margin-top: 0;
-            margin-bottom: 20px;
-            color: white;
-            }
-        
-        body{
-            background-image: url("fond_login.png");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-            font-family: 'Roboto', sans-serif;
-            background-attachment: fixed;
-            color: white;
-            }
-        a {
-            background-color: black;
-            color: white;
-            border-radius: 20px; 
-            padding: 10px 20px; 
-            border: none;
-            text-decoration: none;
-            }
-
-        a:hover {
-            background-color: white;
-            color: black;
-            border-radius: 20px; 
-            padding: 10px 20px; 
-            border: none;
-            text-decoration: none;
-            }
-
-        form{
-            text-align: center;
-            align-items: center;
-        }
-        
-    </style>
+    <link rel="stylesheet" href="styless.css">
 </head>
 <body>
-    <div id="connexion-section">
-    
-<?php
-// Connexion à la base de données MySQL
-$servername = "mysqld";
-$username = "root";
-$password = "Yanis.0311";
-$dbname = "";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connexion à la base de données échouée: " . $conn->connect_error);
-}
-
-// Vérification des données du formulaire
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    $mot_de_passe = $_POST["mot_de_passe"];
-
-    // Vérification dans la base de données
-    $sql = "SELECT * FROM utilisateurs WHERE email='$email' AND mot_de_passe='$mot_de_passe'";
-    $resultat = $conn->query($sql);
-
-    if ($resultat->num_rows == 1) {
-        // Si les données correspondent, renvoie une page HTML de succès
-        echo "<a href='#'>Retoutner à la page d'accuei</a>";
-    } else {
-        // Si les données ne correspondent pas, renvoie une page HTML d'erreur
-        echo "<a href='#'>Retoutner à la page de connexion</a>";
-    }
-}
-
-// Fermeture de la connexion à la base de données
-$conn->close();
-?>
-
-    </div>
+   <form>
+    <fieldset>
+            <h2>Connexion réussi !!</h2>
+           <p>Vous êtes désormais connecter a notre site.</p>
+           <button><a href="Page accueil 2.html">Retourner a l'accueil</a></button>
+     </fieldset>
+    </form>
 </body>
 </html>
     
